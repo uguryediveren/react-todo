324e4f0de517cc22d9bdf08230dc9b5d83863d11
Code Sandbox <hello@codesandbox.io>
2021-10-31 08:03:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d753f82b334486a2f22004cbf974be94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f513f9b5e031d6e79993c5e8f2415c72",
+    "url": "https://uguryediveren.github.io/csb-omscs/index.html"
+  },
+  {
+    "revision": "7eaa903a6e9ea839004f",
+    "url": "https://uguryediveren.github.io/csb-omscs/static/css/main.a3f7e8e8.chunk.css"
+  },
+  {
+    "revision": "0cacaba1b189fe408554",
+    "url": "https://uguryediveren.github.io/csb-omscs/static/js/2.7a4d40a7.chunk.js"
+  },
+  {
+    "revision": "7eaa903a6e9ea839004f",
+    "url": "https://uguryediveren.github.io/csb-omscs/static/js/main.e52292d8.chunk.js"
+  },
+  {
+    "revision": "69393bac9575eb8ae9c9",
+    "url": "https://uguryediveren.github.io/csb-omscs/static/js/runtime~main.964f6a65.js"
+  }
+]);
